1c3978312a119bf1d2b84cd92a58f661c58eae15
Abhay Singh Jamwal <abhayjamwal17@gmail.com>
2020-10-14 22:07:40 +0530

    Updates

---
+++ precache-manifest.fdef6eeb367f44b49f659e236bc0c644.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c68dd65c3e9d06f461d011221d002a3",
+    "url": "/covid-19-app/index.html"
+  },
+  {
+    "revision": "eab11331ce34c3322d2d",
+    "url": "/covid-19-app/static/js/2.c94cc961.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/covid-19-app/static/js/2.c94cc961.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "145e9ad519c941e82698",
+    "url": "/covid-19-app/static/js/main.edfe9a30.chunk.js"
+  },
+  {
+    "revision": "91a457564a798b12bc69",
+    "url": "/covid-19-app/static/js/runtime-main.1ad87898.js"
+  }
+]);
